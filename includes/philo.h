25856375cff79188906706b74f03db843a7cb34f
Eduardo Castro <eduardocastrobritoo@gmail.com>
2024-06-15 23:19:27 -0300

    init header

---
+++ includes/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: educastro <educastro@student.42.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/15 22:02:59 by educastro         #+#    #+#             */
+/*   Updated: 2024/06/15 22:08:28 by educastro        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+// macros
+# define THREADS 5
+
+#endif
